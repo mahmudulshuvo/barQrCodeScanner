2b90eaf446d978bd453b1af58ba900662b1f0ff0
mahmudulshuvo <shuvo0201@gmail.com>
2017-08-08 15:14:10 +0600

    Initial Commit.

--- QRBRCodeScanner/QRCodeViewController.swift
+++ QRBRCodeScanner/QRCodeViewController.swift
@@ -1,6 +1,6 @@
 //
-//  ViewController.swift
-//  QRBRCodeScanner
+//  QRCodeViewController.swift
+//  QRCodeReader
 //
 //  Created by Shuvo on 8/8/17.
 //  Copyright © 2017 SHUVO. All rights reserved.
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QRCodeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // MARK: - Navigation
 
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
-
---
+++ README.md
@@ -0,0 +1,4 @@
+## barQrCodeScanner
+
+It is an iOS application to demonstrate how to use AVFoundation framework to scan QR and Barcodes.
+
